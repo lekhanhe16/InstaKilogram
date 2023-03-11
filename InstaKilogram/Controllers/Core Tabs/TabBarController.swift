41336eae1ac65640796ce4b4e0ffe943413ef92d
lekhanhe16 <55609697+lekhanhe16@users.noreply.github.com>
2023-03-11 11:30:08 +0700

    First commit

---
+++ InstaKilogram/Controllers/Core Tabs/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaKilogram
+//
+//  Created by acupofstarbugs on 11/03/2023.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
